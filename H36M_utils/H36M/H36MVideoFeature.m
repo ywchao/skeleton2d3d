8f7c2212d0924c50f40d5f985fd89081e064e328
ywchao <ywchao@umich.edu>
2018-07-03 09:21:56 -0400

    Clean up Human3.6M dir and utils

--- H36M_utils/H36M/H36MVideoFeature.m
+++
@@ -1,3 +0,0 @@
-classdef H36MVideoFeature < H36MFeature
-    % FIXME bring here all the code for resize
-end
--- addPaths.m
+++ addPaths.m
@@ -1,10 +1,9 @@
-addpath('H36M_utils/H36M');
-addpath('H36M_utils/H36MBL');
-addpath('H36M_utils/utils');
-
-addpath('H36M_utils/external_utils');
-addpath('H36M_utils/external_utils/lawrennd-mocap');
-addpath('H36M_utils/external_utils/xml_io_tools');
+addpath('h36m_utils/Release-v1.1/external_utils');
+addpath('h36m_utils/Release-v1.1/external_utils/xml_io_tools');
+addpath('h36m_utils/Release-v1.1/H36M');
+addpath('h36m_utils/Release-v1.1/H36MBL');
+addpath('h36m_utils/Release-v1.1/utils');
+addpath('h36m_utils/lawrennd-mocap');
 
 addpath('common');
 addpath('paper');
